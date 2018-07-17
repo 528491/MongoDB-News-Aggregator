57afd8a2a735a0c13b3917180d1abae5a27454ff
528491 <jacobminasian@gmail.com>
2018-07-16 23:36:57 -0700

    Commenced work on web scraping using cheerio and request

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,23 @@
+var cheerio = require("cheerio");
+var request = require("request");
+
+
+
 module.exports.index = function(req, res){
+    
+    request("https://www.cnn.com/", function(error, response, html){
+        var $ = cheerio.load(html);
+        var results = [];
+
+        $("span").each(function(i, element){
+            console.log(element);
+        });
+    });
+    
+    
+    
+    
+    
+    
     res.render('home');
 }
